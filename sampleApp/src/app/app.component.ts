b6e518309bf7835512cffc224472e58eebd85515
Lerisa <lerisa.gomes@netwealth.com>
2024-10-23 18:40:37 +0100

    feat: create roles and designation components

--- sampleApp/src/app/app.component.ts
+++ sampleApp/src/app/app.component.ts
@@ -2,11 +2,13 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { of } from 'rxjs';
+import { DesignationComponent } from './components/designation/designation.component';
+import { RolesComponent } from './components/roles/roles.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet, CommonModule, RolesComponent, DesignationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
