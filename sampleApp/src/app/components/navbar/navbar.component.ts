8ab98ee8a3827f19f50df4e0ff7d9b1bbebf3ac5
Lerisa <lerisa.gomes@netwealth.com>
2024-11-22 18:55:01 +0000

    navbar without bootstrap

--- sampleApp/src/app/components/navbar/navbar.component.ts
+++ sampleApp/src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [RouterLinkActive, RouterOutlet, RouterLink, CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  menuOpen = false;
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
+}
